684ea7d3f0cca263127195888a5370cfacbcddc3
Daniel <daniel.agostinho@hotmail.com>
2021-01-16 21:32:36 -0300

    design pattern template method

---
+++ template-method/src/main/java/br/com/design/pattern/templatemethod/orcamento/Orcamento.java
@@ -0,0 +1,13 @@
+package br.com.design.pattern.templatemethod.orcamento;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+
+@Getter
+@AllArgsConstructor
+public class Orcamento {
+    private BigDecimal valor;
+    private int quantidadeDeItens;
+}
